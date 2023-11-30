ed3c1d7af57f58027abce68b6a29b2dfa5d01aca
dipam-virani <dipamvirani@gmail.com>
2023-11-30 14:24:41 -0500

    Merge changes from remote

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dipam-virani/hostel-management-system-project',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0d7a929fe4c70bd964b3213f6f165ce5de92b3ec',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dipam-virani/hostel-management-system-project' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0d7a929fe4c70bd964b3213f6f165ce5de92b3ec',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
